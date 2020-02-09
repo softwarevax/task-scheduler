f655c91e96841c2ba79ff0dbb53eebae3ab84133
softwarevax <1244497026@qq.com>
2020-02-09 20:32:22 +0800

    1、bug修复

--- 2019-nCoV/src/main/java/org/platform/quartz/ncov/web/service/HistoryService.java
+++ 2019-nCoV/src/main/java/org/platform/quartz/ncov/web/service/HistoryService.java
@@ -4,8 +4,6 @@ import org.platform.quartz.ncov.configuration.SenderListener;
 import org.platform.quartz.ncov.configuration.Subject;
 import org.platform.quartz.ncov.domain.History;
 import org.platform.quartz.ncov.web.dao.HistoryDao;
-import org.platform.quartz.utils.date.DateUitls;
-import org.platform.quartz.utils.langs.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
 
@@ -30,21 +28,16 @@ public class HistoryService implements Subject {
         if(CollectionUtils.isEmpty(histories)) {
             return;
         }
-        List<String> dates = historyDao.getDates();
         for (History h : histories) {
-            if(dates.contains(h.getDate()) && !StringUtils.equals(DateUitls.getDate(), h.getDate())) {
-                // 如果日期存在，且不是今天，表明是历史数据, 历史数据不会修改
-                continue;
+            String date = h.getDate();
+            History history  =  historyDao.getByDate(date);
+            if(history != null) {
+                h.setId(history.getId());
             }
-            if(StringUtils.equals(DateUitls.getDate(), h.getDate()))  {
-                // 如果是今天
-                History today  =  historyDao.getByDate(DateUitls.getDate());
-                h.setId(today.getId());
-                if(!today.equals(h)) {
-                    notify(h.toString());
-                }
+            if(history == null || !history.equals(h)) {
+                notify(h.toString());
+                historyDao.saveAndFlush(h);
             }
-            historyDao.saveAndFlush(h);
         }
     }
 
